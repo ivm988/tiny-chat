bced64258726b6f1d29ad78affd7ef78350bf88d
Ivan Misnik <ivmisnik@gmail.com>
2017-03-24 21:49:10 +0300

    made base frame of application

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,35 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Messages } from '../api/messages.js';
 
 import Message from './Message.jsx';
 
-export default class App extends Component {
-
-    getMessages() {
-    return [
-      { _id: 1, text: 'This is message 1' },
-      { _id: 2, text: 'This is message 2' },
-      { _id: 3, text: 'This is message 3' },
-    ];
-  }
+class App extends Component {
 
   renderMessages() {
-    return this.getMessages().map((message) => (
+    return this.props.messages.map((message) => (
       <Message key={message._id} message={message} />
     ));
   }
 
   render() {
     return (
-      <div className="container">
-          <div className="row">
-              <div className="col m12">
-                  <h1>Tiny Chat (:</h1>
-              </div>
-          </div>
-          <div className="row">
-              {this.renderMessages()}
-          </div>
-      </div>
+        <div>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col m12">
+                            <h1>Tiny Chat (:</h1>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="container">
+                    <div className="row">
+                        {this.renderMessages()}
+                    </div>
+                </div>
+            </main>
+            <footer className="page-footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col m12">
+
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
     );
   }
 }
+
+App.propTypes = {
+    messages: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+    return {
+        messages: Messages.find({}).fetch()
+    };
+}, App);
